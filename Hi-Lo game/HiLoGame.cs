3fbef88da6c9d3ea3cee5555c2d3fb368250f67b
Muayad2024 <muayadtrade@gmail.com>
2026-02-12 20:23:00 +0800

    Add project files.

---
+++ Hi-Lo game/HiLoGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hi_Lo_game
+{
+    static class HiLoGame     //all the members are static
+    {
+        public const int MAXIMUM = 10;    //this is a constant and no one can change it
+        private static int currentNumber = Random.Shared.Next(1, MAXIMUM + 1);
+        private static int nextNumber = Random.Shared.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        public static int GetPot() { return pot; }
+
+        public static void Guess(bool higher)
+        {
+            if ((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+                
+
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong."); 
+                pot--;
+
+            }
+            currentNumber = nextNumber;
+            nextNumber = Random.Shared.Next(1, MAXIMUM + 1);
+            Console.WriteLine($"The current number is {currentNumber}");
+
+        }
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (nextNumber >= half)
+            {
+                Console.WriteLine($"The current number is {currentNumber}," + $" the next number is at least {half}");
+            }
+            else
+            {
+                Console.WriteLine($"The current number is {currentNumber}," +
+                $" the next is at most {half}");
+            }
+            pot--;
+        }
+
+
+    }
+}
---
+++ Hi-Lo game/Program.cs
@@ -0,0 +1,22 @@
+﻿using Hi_Lo_game;
+/*This game the player will guess two numbers
+ * that generated randomly and if they are higher or lower 
+ * and if his guess is true it will increase the pot by one mark 
+ * and if the player give wrong answer the pot will decrease by one mark
+ */
+Console.WriteLine("Welcome to HiLo.");
+Console.WriteLine($"Guess numbers between 1 and {HiLoGame.MAXIMUM}.");
+HiLoGame.Hint();
+while (HiLoGame.GetPot() > 0)
+{
+    int potValue = HiLoGame.GetPot();
+    Console.WriteLine($"The pot Value is {potValue}");
+    Console.WriteLine("Press h for higher, l for lower, ? to buy a hint,");
+    Console.WriteLine($"or any other key to quit");
+    char key = Console.ReadKey(true).KeyChar;
+    if (key == 'h') HiLoGame.Guess(true);
+    else if (key == 'l') HiLoGame.Guess(false);
+    else if (key == '?') HiLoGame.Hint();
+    else return;
+}
+Console.WriteLine("The pot is empty. Bye!");
