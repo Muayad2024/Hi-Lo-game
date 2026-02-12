3a78eb928fceb36bf6472fbdbf5f2d440df026e8
Muayad2024 <muayadtrade@gmail.com>
2026-02-12 20:23:00 +0800

    Add project files.

---
+++ Hi-Lo game/Program.cs
@@ -0,0 +1,20 @@
+﻿using Hi_Lo_game;
+/*This game the player will guess two numbers
+ * that generated randomly and if they are higher or lower 
+ * and if his guess is true it will increase the pot by one mark 
+ */
+Console.WriteLine("Welcome to HiLo.");
+Console.WriteLine($"Guess numbers between 1 and {HiLoGame.MAXIMUM}.");
+HiLoGame.Hint();
+while (HiLoGame.GetPot() > 0)
+{
+    Console.WriteLine($"The pot amount is {HiLoGame.pot}");
+    Console.WriteLine("Press h for higher, l for lower, ? to buy a hint,");
+    Console.WriteLine($"or any other key to quit with {HiLoGame.GetPot()}.");
+    char key = Console.ReadKey(true).KeyChar;
+    if (key == 'h') HiLoGame.Guess(true);
+    else if (key == 'l') HiLoGame.Guess(false);
+    else if (key == '?') HiLoGame.Hint();
+    else return;
+}
+Console.WriteLine("The pot is empty. Bye!");
